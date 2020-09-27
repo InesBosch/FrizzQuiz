296b9fcdcedc9f58975e158894fe58c254647c93
Ines Bosch-Alfonso <00monserrat@gmail.com>
2020-09-27 19:16:29 -0400

    Updates

---
+++ precache-manifest.f2f48240877c81b7937309d3318054e4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5746bfd76d239142c7ef27841b23aa1",
+    "url": "/FrizzQuiz/index.html"
+  },
+  {
+    "revision": "89ef08b22d93cdcb1701",
+    "url": "/FrizzQuiz/static/css/main.b70b619c.chunk.css"
+  },
+  {
+    "revision": "947e9bfed25d0d7e5720",
+    "url": "/FrizzQuiz/static/js/2.0511f812.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/FrizzQuiz/static/js/2.0511f812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ef08b22d93cdcb1701",
+    "url": "/FrizzQuiz/static/js/main.8a647406.chunk.js"
+  },
+  {
+    "revision": "6454caaef12621066947",
+    "url": "/FrizzQuiz/static/js/runtime-main.6ac0967b.js"
+  },
+  {
+    "revision": "c4bde20670912ef3779b3fb578715f87",
+    "url": "/FrizzQuiz/static/media/curly.c4bde206.png"
+  },
+  {
+    "revision": "5189022258c5a3a5092584e98c5cdec1",
+    "url": "/FrizzQuiz/static/media/idk.51890222.png"
+  },
+  {
+    "revision": "9d42c51e0dfe551b6eea60999698e029",
+    "url": "/FrizzQuiz/static/media/shock.9d42c51e.png"
+  }
+]);
